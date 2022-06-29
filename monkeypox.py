3266acf62f9902ff02eee170bedbf70376094499
ten14301 <49758110+ten14301@users.noreply.github.com>
2022-06-29 19:33:45 +0700

    Add files via upload

---
+++ monkeypox.py
@@ -0,0 +1,32 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+request = requests.get('https://www.bbc.co.uk/search?q=monkeypox&page=1')
+html = request.content
+sup = BeautifulSoup(html, 'html.parser')
+Linetoken = "Line-token"
+
+
+def LineNotifyMessage(message):
+    payload = {'message':message}
+    return LineNotify(payload)
+def LineNotify(payload,file=None):
+    import requests
+    url = 'https://notify-api.line.me/api/notify'
+    token = Linetoken
+    headers = {'Authorization':'Bearer ' + token}
+    return requests.post(url, headers=headers , data = payload, files=file)
+
+def monkeypoxcheck():
+    head = sup.findAll('div',{'class' : 'ssrcss-1f3bvyz-Stack'})
+    
+    for div in head:
+      links = div.findAll('a')
+      for a in links:
+         LineNotifyMessage("ข่าวล่าสุดเรื่องฝีดาษลิง จาก BBC NEWS UK\n\n" + a.get_text() + "\nอ่านรายละเอียดเพิ่มเติมได้ที่ " + a['href'] + "\n")
+            
+
+ 
+        
+monkeypoxcheck()
